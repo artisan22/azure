aeef240fd72ac208f43e3ee9f3fff247fad84e38
artisan22 <vineet.shukla@outlook.com>
2026-01-14 11:29:19 +1100

    Set location default to australiaeast

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_cidr]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  for_each = { for subnet in var.subnets : subnet.name => subnet }
+
+  name                 = each.value.name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [each.value.cidr]
+}
